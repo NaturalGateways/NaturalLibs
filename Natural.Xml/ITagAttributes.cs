0c2c86b7f86d7775addf28d239546c41bf400b8b
Steven Moore <steve.natgate@gmail.com>
2023-07-10 21:51:36 +1000

    Xml: Added new attribute readers

--- Natural.Xml/ITagAttributes.cs
+++ Natural.Xml/ITagAttributes.cs
@@ -11,10 +11,16 @@ namespace Natural.Xml
     {
         /// <summary>Getter for a string, throws an exception if it is missing.</summary>
         string GetString(string name);
+        /// <summary>Getter for a long integer, throws an exception if it is missing or invalid.</summary>
+        long GetLong(string name);
+        /// <summary>Getter for an enum, throws an exception if it is missing or invalid.</summary>
+        EnumType GetEnum<EnumType>(string name) where EnumType : struct, IConvertible;
 
         /// <summary>Getter for a string, returns null if it is missing.</summary>
         string? GetNullableString(string name);
-        /// <summary>Getter for an enum, returns null if it is missing.</summary>
+        /// <summary>Getter for a long integer, returns null if it is missing or invalid.</summary>
+        long? GetNullableLong(string name);
+        /// <summary>Getter for an enum, returns null if it is missing or invalid.</summary>
         EnumType? GetNullableEnum<EnumType>(string name) where EnumType : struct, IConvertible;
     }
 }
--- Natural.Xml/InternalObjects/XmlAttributes.cs
+++ Natural.Xml/InternalObjects/XmlAttributes.cs
@@ -32,6 +32,30 @@ namespace Natural.Xml
             return attributeValue;
         }
 
+        /// <summary>Getter for a long integer, throws an exception if it is missing or invalid.</summary>
+        public long GetLong(string name)
+        {
+            string? attributeValue = m_reader.GetAttribute(name);
+            if (attributeValue == null)
+                throw new Exception($"Tag '{m_reader.Name}' has no attribute '{name}'.");
+            long longValue = 0;
+            if (Int64.TryParse(attributeValue, out longValue))
+                return longValue;
+            throw new Exception($"Tag '{m_reader.Name}' has invalid attribute '{name}': '{attributeValue}'");
+        }
+
+        /// <summary>Getter for an enum, throws an exception if it is missing or invalid.</summary>
+        public EnumType GetEnum<EnumType>(string name) where EnumType : struct, IConvertible
+        {
+            string? attributeValue = m_reader.GetAttribute(name);
+            if (attributeValue == null)
+                throw new Exception($"Tag '{m_reader.Name}' has no attribute '{name}'.");
+            EnumType enumValue = default(EnumType);
+            if (Enum.TryParse<EnumType>(attributeValue, out enumValue))
+                return enumValue;
+            throw new Exception($"Tag '{m_reader.Name}' has invalid attribute '{name}': '{attributeValue}'");
+        }
+
         /// <summary>Getter for a string, returns null if it is missing.</summary>
         public string? GetNullableString(string name)
         {
@@ -41,7 +65,19 @@ namespace Natural.Xml
             return attributeValue;
         }
 
-        /// <summary>Getter for an enum, returns null if it is missing.</summary>
+        /// <summary>Getter for a long integer, returns null if it is missing or invalid.</summary>
+        public long? GetNullableLong(string name)
+        {
+            string? attributeValue = m_reader.GetAttribute(name);
+            if (attributeValue == null)
+                return null;
+            long longValue = 0;
+            if (Int64.TryParse(attributeValue, out longValue))
+                return longValue;
+            return null;
+        }
+
+        /// <summary>Getter for an enum, returns null if it is missing or invalid.</summary>
         public EnumType? GetNullableEnum<EnumType>(string name) where EnumType : struct, IConvertible
         {
             string? attributeValue = m_reader.GetAttribute(name);
@@ -49,9 +85,7 @@ namespace Natural.Xml
                 return null;
             EnumType enumValue = default(EnumType);
             if (Enum.TryParse<EnumType>(attributeValue, out enumValue))
-            {
                 return enumValue;
-            }
             return null;
         }
 
